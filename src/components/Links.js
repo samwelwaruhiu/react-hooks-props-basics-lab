670040b660cabab4cfd6fb669cff45a7cafc44bc
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-09-21 16:01:57 +0300

    complited assignment

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(){
+    return(
+    <div>
+    <h3>Links</h3>
+    <a href="{props.github}">https://github.com/liza</a>
+    <a href="{props.linkedin}">https://www.linkedin.com/in/liza/</a>
+    </div>
+    )
+}
+
+export default Links;
